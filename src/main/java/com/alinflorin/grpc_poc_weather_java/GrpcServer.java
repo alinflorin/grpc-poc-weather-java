5b2e2260ee3a02cb6a780b496f947a0ded2a08df
Alin Ciu <alin.ciu@endava.com>
2020-07-10 00:50:59 +0300

    -

--- src/main/java/com/alinflorin/grpc_poc_weather_java/GrpcServer.java
+++ src/main/java/com/alinflorin/grpc_poc_weather_java/GrpcServer.java
@@ -6,41 +6,52 @@ import java.util.logging.Logger;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import com.alinflorin.grpc_poc_weather_java.grpc.*;
 
 public class GrpcServer {
-    private static final Logger logger = Logger.getLogger(GrpcServer.class.getName());
-    private Server server;
+  private static final Logger logger = Logger.getLogger(GrpcServer.class.getName());
+  private Server server;
 
-    public void start() throws IOException {
-        int port = 9090;
-        server = ServerBuilder.forPort(port)
-            // .addService(new GreeterImpl())
-            .build()
-            .start();
-        logger.info("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-          @Override
-          public void run() {
-            System.err.println("*** shutting down gRPC server since JVM is shutting down");
-            try {
-                GrpcServer.this.stop();
-            } catch (InterruptedException e) {
-              e.printStackTrace(System.err);
-            }
-            System.err.println("*** server shut down");
-          }
-        });
-      }
-    
-      public void stop() throws InterruptedException {
-        if (server != null) {
-          server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+  public void start() throws IOException {
+    int port = 9090;
+    server = ServerBuilder.forPort(port)
+        .addService(new WeatherService())
+        .build().start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        try {
+          GrpcServer.this.stop();
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
         }
+        System.err.println("*** server shut down");
       }
+    });
+  }
 
-      public void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-          server.awaitTermination();
-        }
-      }
+  public void stop() throws InterruptedException {
+    if (server != null) {
+      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+  }
+
+  public void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  static class WeatherService extends WeatherGrpc.WeatherImplBase {
+    @Override
+    public void getWeather(GetWeatherRequest request, StreamObserver<GetWeatherReply> responseObserver) {
+      GetWeatherReply reply = GetWeatherReply.newBuilder().setCurrentTemp(1).setCurrentWindSpeed(2).build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+  }
 }
